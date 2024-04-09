17f9ae368d91833f8a9c35dbf3da10d382fe81ed
Lukyous <70490863+Lukyous@users.noreply.github.com>
2024-04-09 17:06:22 +0200

    Update chiffrement.py

--- chiffrement.py
+++ chiffrement.py
@@ -13,4 +13,16 @@ def chiffrement(message):
     for i in message:
         message = message.replace(i, dictio[i])
     return f"cle=\t{cle}\nchiffrement=\t{message}"
-print(chiffrement("salut"))
+
+def dechiffrement(message_chiffre, cle):
+    dictio={}
+    carac = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
+    for i in cle:
+        r = cle.index(i)
+        dictio[i] = f"{carac[r]}"
+    for i in message_chiffre:
+        message_chiffre = message_chiffre.replace(i, dictio[i])
+    return f"déchiffrement:{message_chiffre}"
+    
+#print(chiffrement("salut"))
+dechiffrement("hL8VK", """La.A^qYyrcX8I+gF[<hKV(nH,smCj]pD!1R_ZPNk'=4x%Sit@0;6>u7vo5O2)3*bU:TWB |G}f{#eMl&$"wJQd9z?/E\-""")
