64623d98dcfabc22cca67ca2888cb2e9a2a65e97
Lukyous <70490863+Lukyous@users.noreply.github.com>
2024-04-10 16:01:47 +0200

    chiffrement.py

--- chiffrement.py
+++ chiffrement.py
@@ -1,9 +1,9 @@
 import random
-
+import subprocess
+import time
 def chiffrement(message):
     dictio = {}
     mess_chiffr=""
-    tx = message
     carac = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|'\";:,<.>/?£¤"
     symbole = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|'\";:,<.>/?£¤")
     random.shuffle(symbole)
@@ -33,7 +33,7 @@ def chiffrement(message):
                 ordre += b
     cle = "".join(liste_div)
     cle += ordre
-    return tx, cle, mess_chiffr
+    return cle, mess_chiffr
 
 def dechiffrement(message_chiffre, li):
     code = li[-38:]
@@ -69,13 +69,48 @@ def dechiffrement(message_chiffre, li):
         message_decode+=dictio[i]
     return message_decode
 
-chi = None 
-dec = None  
-chi = chiffrement("salut")
-if chi is not None:
-    print(f"message demandé à chiffrer: {chi[0]}")
-    print(f"clé de chiffrement: {chi[1]}")
-    print(f"message chiffré: {chi[2]}")
-    dec = dechiffrement(chi[2], chi[1])
-    print("\n")
-    print(f"message déchiffré: {dec}")
+def menu():
+    print("""
+-----------------------------------------------------------------------------------------------------------------------------      
+|     _          _                                                  _      _     ___     ___                                 |
+|    | |        | |                                                | |    (_)   / __)   / __)                                |
+|    | |  _   _ | |  _  _   _   ___   _   _   ___             ____ | |__   _  _| |__  _| |__   ____  _____   ____  _____     |
+|    | | | | | || |_/ )| | | | / _ \ | | | | /___)           / ___)|  _ \ | |(_   __)(_   __) / ___)(____ | / _  || ___ |    |
+|    | | | |_| ||  _ ( | |_| || |_| || |_| ||___ |          ( (___ | | | || |  | |     | |   | |    / ___ |( (_| || ____|    |
+|     \_)|____/ |_| \_) \__  | \___/ |____/ (___/            \____)|_| |_||_|  |_|     |_|   |_|    \_____| \___ ||_____)    |
+|                      (____/                                                                              (_____|           |
+------------------------------------------------------------------------------------------------------------------------------  
+""")
+    print("\n1) Chiffrer\n2) Déchiffrer")
+    choix = int(input("Quel est votre choix?").strip())
+    if choix == 1:
+        txt = input("que souhaitez-vous chiffrer? ")
+        chi = chiffrement(txt)
+        print(f"clé de chiffrement: {chi[0]}")
+        print(f"message chiffré: {chi[1]}")
+        txt=""
+        input("Cliquer sur entrer pour retourner au menu: ")
+        subprocess.run("cls", shell=True)
+        menu()
+    elif choix == 2:
+        txt = input("test à déchiffrer: ")
+        cle = input("Clé de déchiffrement: ")
+        dec = dechiffrement(txt, cle)
+        print("\n")
+        print(f"message déchiffré: {dec}")
+        input("Cliquer sur entrer pour retourner au menu: ")
+        subprocess.run("cls", shell=True)
+        menu()
+    else:
+        print("Erreur, vous pouvez choisir soit 1, soit 2.")
+        u=3
+        for i in range(3):
+            print(u, end="", flush=True)
+            time.sleep(1)
+            print("\b \b", end="", flush=True)
+            u -= 1
+        subprocess.run("cls", shell=True)
+        u=3
+        menu()
+
+menu()
