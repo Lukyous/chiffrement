45bf18896fb802b643397a759b4f5b6c0dceda41
Lukyous <70490863+Lukyous@users.noreply.github.com>
2024-04-10 14:29:51 +0200

    Update chiffrement.py

--- chiffrement.py
+++ chiffrement.py
@@ -2,27 +2,80 @@ import random
 
 def chiffrement(message):
     dictio = {}
-    chiffrage = ""
-    carac = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
-    symbole = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|;:'\",<.>/?")
+    mess_chiffr=""
+    tx = message
+    carac = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|'\";:,<.>/?£¤"
+    symbole = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|'\";:,<.>/?£¤")
     random.shuffle(symbole)
     cle = "".join(symbole)
     for i in carac:
         r = carac.index(i)
         dictio[i] = f"{symbole[r]}"
     for i in message:
-        message = message.replace(i, dictio[i])
-    return f"cle=\t{cle}\nchiffrement=\t{message}"
+        mess_chiffr += dictio[i]
+    tp = cle
+    car = ""
+    ordre = ""
+    liste_princ = []
+    for i in cle:
+        car += i
+        if len(car) == 5:
+            liste_princ.append(car)
+            car = ""
+    liste_div = liste_princ.copy()
+    random.shuffle(liste_div)
+    for i in liste_princ:
+        for u in liste_div:
+            if i == u:
+                b = str(liste_div.index(u))
+                if len(b) < 2:
+                    b = "0"+b
+                ordre += b
+    cle = "".join(liste_div)
+    cle += ordre
+    return tx, cle, mess_chiffr
 
-def dechiffrement(message_chiffre, cle):
+def dechiffrement(message_chiffre, li):
+    code = li[-38:]
+    li = li[:-38]
+    lu = []
+    a = ""
+    for i in li:
+        a += i
+        if len(a) == 5:
+            lu.append(a)
+            a = ""
+    liste = []
+    intliste = []
+    refaite = []
+    rs = ""
+    for i in code:
+        rs += i
+        if len(rs) == 2:
+            liste.append(rs)
+            rs=""  
+    for i in liste:
+        intliste.append(int(i))
+    for i in intliste:
+        refaite.append(lu[i])
+    cle = "".join(refaite)
     dictio={}
-    carac = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|;:'\",<.>/?"
+    carac = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 !@#$%^&*()-_=+[{]}\\|'\";:,<.>/?£¤"
+    message_decode=""
     for i in cle:
         r = cle.index(i)
         dictio[i] = f"{carac[r]}"
     for i in message_chiffre:
-        message_chiffre = message_chiffre.replace(i, dictio[i])
-    return f"déchiffrement:{message_chiffre}"
-    
-#print(chiffrement("salut"))
-dechiffrement("hL8VK", """La.A^qYyrcX8I+gF[<hKV(nH,smCj]pD!1R_ZPNk'=4x%Sit@0;6>u7vo5O2)3*bU:TWB |G}f{#eMl&$"wJQd9z?/E\-""")
+        message_decode+=dictio[i]
+    return message_decode
+
+chi = None 
+dec = None  
+chi = chiffrement("salut")
+if chi is not None:
+    print(f"message demandé à chiffrer: {chi[0]}")
+    print(f"clé de chiffrement: {chi[1]}")
+    print(f"message chiffré: {chi[2]}")
+    dec = dechiffrement(chi[2], chi[1])
+    print("\n")
+    print(f"message déchiffré: {dec}")
